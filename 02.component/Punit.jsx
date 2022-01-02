a4351b8a6a232d55cbd989d240f730213822a94b
Punit Katiyar <punit.kat@gmail.com>
2022-01-02 17:51:36 +0530

    Add files via upload

---
+++ 02.component/Punit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Punit(props) {
+    return (
+        <div>
+            <h1>Hello {props.name}</h1>
+            <h2>Ducat india</h2>
+        </div >
+    )
+}
+
