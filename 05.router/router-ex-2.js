14e74180ce2b7c643d3e309c41b087ab0ffd561b
Punit Katiyar <punit.kat@gmail.com>
2023-04-23 14:21:39 +0530

    Update and rename router-ex-2.js to router-ex-2.html

--- 05.router/router-ex-2.js
+++
@@ -1,8 +0,0 @@
-<BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/page" element={<Page/>}/>
-          <Route path="/post" element={<Post/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-        </Routes>
-</BrowserRouter>
