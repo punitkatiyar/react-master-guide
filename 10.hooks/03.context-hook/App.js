d57c917610d0b65b6e2345e1ccbbf27bc5403115
Punit Katiyar <punit.kat@gmail.com>
2023-05-20 13:41:31 +0530

    Add files via upload

---
+++ 10.hooks/03.context-hook/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ComponentC from "./Context/ComponentC";
+
+export const UserContext = React.createContext()
+export const UserChanel = React.createContext()
+
+function App() {
+  return (
+    <div>
+      <UserContext.Provider value={'Punit'}>
+        <UserChanel.Provider value="Ducat">
+            <ComponentC />
+          </UserChanel.Provider>
+      </UserContext.Provider>
+    </div>
+  )
+}
+export default App;
