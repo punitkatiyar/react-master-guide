d25c08472c68bc62441a90f4a6a0d0fb69a87d56
Punit Katiyar <punit.kat@gmail.com>
2023-08-05 18:38:00 +0530

    ok

--- 10.hooks/03.context-hook/ComponentE.js
+++ 10.hooks/03.context-hook/ComponentE.js
@@ -1,13 +1,11 @@
 import React, {useContext } from 'react'
-
 import { UserContext,UserChanel } from '../App'
+
+
 function ComponentE() {
 
     const user = useContext(UserContext)
     const chanel= useContext(UserChanel)
-
-
-
   return (
       <div>
           {user} { chanel}
