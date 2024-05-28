93e9c1a2ede7c9f53c849f35afb48e13bbb66469
Punit Katiyar <punit.kat@gmail.com>
2024-05-28 13:30:43 +0530

    Update 💬

--- 08.local-storage/Home.js
+++ 08.local-storage/Home.js
@@ -1,30 +1,14 @@
 import React, { useState } from 'react'
-
 function Home() {
-  
    const handleClick=()=>{
     localStorage.clear();
     window.location.reload()
    }
-    
-
   return (
     <>
    <h1>Profile Page</h1>
    <button onClick={handleClick}>Logout</button>
-   
-
-
-
-
     </>
-
-
-
-
-
-
   )
 }
-
 export default Home
