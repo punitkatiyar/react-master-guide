159495ff9a8a0f0a9c43d22e16758a795cb27392
Punit Katiyar <punit.kat@gmail.com>
2025-03-19 17:21:41 +0530

    update

---
+++ 06.react-hooks/04.useReduce-hook/CounterTwo.jsx
@@ -0,0 +1,36 @@
+import React, { useReducer } from 'react'
+const initialState = {
+    firstCounter: 0,
+    secondCounter: 10
+}
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return { ...state, firstCounter: state.firstCounter + action.value }
+        case 'decrement':
+            return { ...state, firstCounter: state.firstCounter - action.value }
+        case 'increment2':
+            return { ...state, secondCounter: state.secondCounter + action.value }
+        case 'decrement2':
+            return { ...state, secondCounter: state.secondCounter - action.value }
+        case 'reset':
+            return initialState
+        default:
+            return state
+    }
+}
+function CounterTwo() {
+    const [count, dispatch] = useReducer(reducer, initialState)
+    return (
+        <div>
+            <div>Count = {count.firstCounter}</div>
+            <div>Count = {count.secondCounter}</div>
+            <button onClick={() => dispatch({ type: 'increment', value: 1 })}>Increment</button>
+            <button onClick={() => dispatch({ type: 'decrement', value: 1 })}>Decrement</button>
+            <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+            <button onClick={() => dispatch({ type: 'increment2', value: 5 })}>Increment 5</button>
+            <button onClick={() => dispatch({ type: 'decrement2', value: 5 })}>Decrement 5</button>
+        </div>
+    )
+}
+export default CounterTwo
